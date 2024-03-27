9c9018947ccb1419aaccf15eed03e49cb7f49107
Kraig Larson <147437185+Z00K00@users.noreply.github.com>
2024-03-27 13:09:34 -0400

    Initial Commit

---
+++ AirBNB/AirBNBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBNBApp.swift
+//  AirBNB
+//
+//  Created by Krajg Larson on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBNBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
